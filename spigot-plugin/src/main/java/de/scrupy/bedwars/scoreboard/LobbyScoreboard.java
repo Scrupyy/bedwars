7d7403e6810dd041d95bbc432af9f3cdcc00b9f6
Scrupyy <jannikbroessel180@gmail.com>
2023-07-30 15:39:14 +0200

    Add selected map to lobby scoreboard

--- spigot-plugin/src/main/java/de/scrupy/bedwars/scoreboard/LobbyScoreboard.java
+++ spigot-plugin/src/main/java/de/scrupy/bedwars/scoreboard/LobbyScoreboard.java
@@ -1,11 +1,15 @@
 package de.scrupy.bedwars.scoreboard;
 
+import de.scrupy.bedwars.Game;
 import de.scrupy.bedwars.config.GameConfig;
+import de.scrupy.common.map.GameMap;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
 
 public class LobbyScoreboard extends GameScoreboard {
     private static final String CURRENT_TEAM_NAME = "currentTeam";
+    private static final String SCOREBOARD_PLACEHOLDER_KEY = "scoreboardPlaceholder";
 
     public LobbyScoreboard() {
         super();
@@ -14,20 +18,32 @@ public class LobbyScoreboard extends GameScoreboard {
     }
 
     private void setScores() {
+        getObjective().getScore("   ").setScore(6);
+        getObjective().getScore(GameConfig.getInstance().getName("scoreboardCurrentTeam")).setScore(5);
+        getObjective().getScore(ChatColor.BLACK + "").setScore(4);
         getObjective().getScore("  ").setScore(3);
-        getObjective().getScore(GameConfig.getInstance().getName("scoreboardCurrentTeam")).setScore(2);
-        getObjective().getScore(ChatColor.BLACK + "").setScore(1);
+        getObjective().getScore(GameConfig.getInstance().getName("scoreboardSelectedMap")).setScore(2);
+        getObjective().getScore(getSelectedMapName()).setScore(1);
         getObjective().getScore(" ").setScore(0);
+    }
+
+    private String getSelectedMapName() {
+        GameMap gameMap = Game.getInstance().getGameMap();
+        if (gameMap != null) {
+            return String.format(GameConfig.getInstance().getName("scoreboardSelectedMapName"), gameMap.getName());
+        } else {
+            return GameConfig.getInstance().getName(SCOREBOARD_PLACEHOLDER_KEY);
+        }
     }
 
     private void addTeams() {
         org.bukkit.scoreboard.Team currentTeam = getScoreboard().registerNewTeam(CURRENT_TEAM_NAME);
-        currentTeam.setPrefix("§4§lX");
+        currentTeam.setPrefix(GameConfig.getInstance().getName(SCOREBOARD_PLACEHOLDER_KEY));
         currentTeam.addEntry(ChatColor.BLACK + "");
     }
 
     public static void updateTeam(Player player, de.scrupy.bedwars.team.Team team) {
-        org.bukkit.scoreboard.Team scoreboardTeam = player.getScoreboard().getTeam(CURRENT_TEAM_NAME);
+        Team scoreboardTeam = player.getScoreboard().getTeam(CURRENT_TEAM_NAME);
         if (scoreboardTeam != null) {
             String prefix = String.format(GameConfig.getInstance().getName("scoreboardCurrentTeamName"), team.getColoredName());
             scoreboardTeam.setPrefix(prefix);
